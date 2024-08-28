d8ceeb0e8440baffb38d8faccf0cfc9518469f7d
Zifeng Yu <yuzifeng@hotmail.com>
2024-08-28 10:29:00 +0800

    simplify the test case

--- tests/query_id_test.go
+++ tests/query_id_test.go
@@ -10,11 +10,10 @@ import (
 )
 
 func TestServerGenerateQueryID(t *testing.T) {
+	var receivedId string
 	var (
-		idCh = make(chan string, 1)
-		ctx  = proton.Context(context.Background(), proton.WithReceiveQueryID(func(id string) {
-			idCh <- id
-			close(idCh)
+		ctx = proton.Context(context.Background(), proton.WithReceiveQueryID(func(id string) {
+			receivedId = id
 		}))
 
 		conn, err = proton.Open(&proton.Options{
@@ -30,41 +29,36 @@ func TestServerGenerateQueryID(t *testing.T) {
 
 	assert.NoError(t, err)
 	if _, err := conn.Query(ctx, "SELECT 123"); assert.NoError(t, err) {
-		received := <-idCh
 		sv, _ := conn.ServerVersion()
 		if sv.Revision >= proto.DBMS_MIN_PROTOCOL_VERSION_WITH_SERVER_GENERATE_UUID {
-			assert.Equal(t, 36, len(received))
+			assert.Equal(t, 36, len(receivedId))
 		}
 	}
 }
 
 func TestClientGenerateQueryID(t *testing.T) {
 
-	var (
-		idCh = make(chan string, 1)
-		id   = "client_query_id"
-		ctx  = proton.Context(context.Background(), proton.WithQueryID(id), proton.WithReceiveQueryID(func(id string) {
-			idCh <- id
-			close(idCh)
-		}))
+	id := "client_query_id"
+	var receivedId string
+	ctx := proton.Context(context.Background(), proton.WithQueryID(id), proton.WithReceiveQueryID(func(id string) {
+		receivedId = id
+	}))
 
-		conn, err = proton.Open(&proton.Options{
-			Addr: []string{"127.0.0.1:8463"},
-			Auth: proton.Auth{
-				Database: "default",
-				Username: "default",
-				Password: "",
-			},
-			MaxOpenConns: 1,
-		})
-	)
+	conn, err := proton.Open(&proton.Options{
+		Addr: []string{"127.0.0.1:8463"},
+		Auth: proton.Auth{
+			Database: "default",
+			Username: "default",
+			Password: "",
+		},
+		MaxOpenConns: 1,
+	})
 
 	assert.NoError(t, err)
 	if _, err := conn.Query(ctx, "SELECT 123"); assert.NoError(t, err) {
-		received := <-idCh
 		sv, _ := conn.ServerVersion()
 		if sv.Revision >= proto.DBMS_MIN_PROTOCOL_VERSION_WITH_SERVER_GENERATE_UUID {
-			assert.Equal(t, id, received)
+			assert.Equal(t, id, receivedId)
 		}
 	}
 }
